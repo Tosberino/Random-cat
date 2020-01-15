9386cfa50630683a0b6a6285d0791750c38e84ed
Tosbs <35867300+Tosberino@users.noreply.github.com>
2020-01-15 23:23:12 +0200

    Add files via upload

---
+++ cpp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+  string eL[] = {">","o","O","0","U","u","@","^","X","$","\bII","\b==","\b--","\b-="}; //Left eye  | Kreisā acs
+  string eR[] = {"<","o","O","0","U","u","@","^","X","$","\bII","\b==","\b--","\b=-"}; //Right eye | Labā acs
+
+  string d[] = {"U","u","Y","o"}; //Nose  |  Deguns
+  string m[] = {"W ","^ ","w ","\bUU "}; //Mouth  | Mute
+
+  srand(time(NULL));
+
+  //Draw the cat  | Uzzīmēt kaķi
+  //VVVVVVVVVV
+  cout << "\n\t^-------^" << endl;
+  int x = rand() % (sizeof(eL)/sizeof(*eL));
+  cout << "\t| " << eL[x] << "   " << eR[x] << " |" << endl;
+  cout << "\t|   " << d[rand() % (sizeof(d)/sizeof(*d))] <<"   |" << endl;
+  cout << "\t|   " << m[rand() % (sizeof(m)/sizeof(*m))] <<"  |" << endl;
+  cout << "\t|       |" << endl;
+  cout << "\t|_o___o_|\n" << endl;
+}
